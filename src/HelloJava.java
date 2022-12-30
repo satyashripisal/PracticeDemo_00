d66309bfb47e0307bef00ff8a9be09f03c1efa5d
Satyashri <satyashripisal06@gmail.com>
2022-12-30 17:05:58 +0530

    demo file added

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -3,8 +3,8 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hi");
-System.out.println("welcome");
+    System.out.println("Hi");
+   System.out.println("welcome");
 	}
 
 }

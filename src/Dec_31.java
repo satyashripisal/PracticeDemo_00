a29a93f38fac856064df52db0769421eebe1ca19
Satyashri <satyashripisal06@gmail.com>
2022-12-31 16:43:36 +0530

    message added

--- src/Dec_31.java
+++ src/Dec_31.java
@@ -4,6 +4,7 @@ public class Dec_31 {
 	public static void main(String[] args) {
 		System.out.println("31 December");
 		System.out.println("tommorrow is sunday");
+				System.out.println("31 December");
 
 	}
 
